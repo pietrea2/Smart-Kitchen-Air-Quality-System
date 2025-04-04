08351eb7bfb68d555aff3cbb473a60571283c832
Divya Sivakumaran <57161682+divyas248@users.noreply.github.com>
2025-04-04 00:43:19 -0400

    Create MqttClient.h

---
+++ sensors/MqttClient.h
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+#include <MQTT.h>
+
+class MqttClient
+{
+public:
+    MqttClient(const char* mqtt_broker_ip, const int mqtt_broker_port);
+    MqttClient(const char* mqtt_broker_ip, const int mqtt_broker_port, String *subscribe_topics, const int num_subscribe_topics);
+    void connect(const char* client_id);
+    void publish_message(const char *topic, const char *msg);
+    void check_connection(const char* client_id);
+    String get_msg();
+    String get_topic();
+    void reset_msg();
+    
+private:
+    MQTTClient _mqtt_client;
+    String *_subscribe_topics;
+    int _num_subscribe_topics;
+};
